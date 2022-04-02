1f5b7b0e60de3666a407698748f4b9efc787677d
xinyijack <cryingsword@outlook.com>
2022-04-03 00:31:07 +0800

    完成腾讯云图床，并且能进行图片上传

--- src/main/java/com/majiang/community/controller/FileController.java
+++ src/main/java/com/majiang/community/controller/FileController.java
@@ -1,9 +1,19 @@
 package com.majiang.community.controller;
 
 import com.majiang.community.DTO.FIleDTO;
+import com.majiang.community.provider.QCloudProvider;
+import com.qcloud.cos.model.UploadResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author jack
@@ -15,9 +25,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class FileController {
 
+    @Autowired
+    private QCloudProvider qCloudProvider;
+
     @ResponseBody
     @RequestMapping("/file/upload")
-    public FIleDTO upload() {
+    public FIleDTO upload(HttpServletRequest request) {
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        MultipartFile file = multipartRequest.getFile("editormd-image-file");
+        try {
+            InputStream inputStream = file.getInputStream();
+            long length = inputStream.available();
+            qCloudProvider.upload("community/" + file.getOriginalFilename(), inputStream, length);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         FIleDTO fileDTO = new FIleDTO();
         fileDTO.setSuccess(1);
         fileDTO.setUrl("/images/wechat.jpeg");
---
+++ src/main/java/com/majiang/community/provider/QCloudProvider.java
@@ -0,0 +1,264 @@
+package com.majiang.community.provider;
+
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.ClientConfig;
+import com.qcloud.cos.auth.BasicCOSCredentials;
+import com.qcloud.cos.auth.BasicSessionCredentials;
+import com.qcloud.cos.auth.COSCredentials;
+import com.qcloud.cos.exception.CosClientException;
+import com.qcloud.cos.exception.CosServiceException;
+import com.qcloud.cos.http.HttpMethodName;
+import com.qcloud.cos.http.HttpProtocol;
+import com.qcloud.cos.model.ObjectMetadata;
+import com.qcloud.cos.model.PutObjectRequest;
+import com.qcloud.cos.model.UploadResult;
+import com.qcloud.cos.region.Region;
+import com.qcloud.cos.transfer.*;
+import com.tencent.cloud.CosStsClient;
+import com.tencent.cloud.Credentials;
+import com.tencent.cloud.Response;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author jack
+ * @version 1.0.0
+ * @ClassName QcloudProvider.java
+ * @Description TODO
+ * @createTime 2022年04月01日 13:54:00
+ */
+@Service
+public class QCloudProvider {
+
+    @Value("${qcloud.region}")
+    private String regionName;
+
+    @Value("${qcloud.bucket.name}")
+    private String bucketName;
+    // 上传对象
+    public UploadResult upload(String key,
+                               InputStream inputStream,
+                               long inputStreamLength) throws CosClientException {
+        // 使用高级接口必须先保证本进程存在一个 TransferManager 实例，如果没有则创建
+        // 详细代码参见本页：高级接口 -> 创建 TransferManager
+        TransferManager transferManager = createTransferManager();
+
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        // 上传的流如果能够获取准确的流长度，则推荐一定填写 content-length
+        // 如果确实没办法获取到，则下面这行可以省略，但同时高级接口也没办法使用分块上传了
+        objectMetadata.setContentLength(inputStreamLength);
+
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, inputStream, objectMetadata);
+
+        try {
+            // 高级接口会返回一个异步结果Upload
+            // 可同步地调用 waitForUploadResult 方法等待上传完成，成功返回UploadResult, 失败抛出异常
+            Upload upload = transferManager.upload(putObjectRequest);
+            showTransferProgress(upload);
+            UploadResult uploadResult = upload.waitForUploadResult();
+            return uploadResult;
+        } catch (CosServiceException e) {
+            e.printStackTrace();
+        } catch (CosClientException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // 确定本进程不再使用 transferManager 实例之后，关闭之
+// 详细代码参见本页：高级接口 -> 关闭 TransferManager
+        shutdownTransferManager(transferManager);
+        return new UploadResult();
+    }
+
+    // 创建 TransferManager 实例，这个实例用来后续调用高级接口
+    TransferManager createTransferManager() {
+        // 创建一个 COSClient 实例，这是访问 COS 服务的基础实例。
+        // 详细代码参见本页: 简单操作 -> 创建 COSClient
+        COSClient cosClient = createCOSClient();
+
+        // 自定义线程池大小，建议在客户端与 COS 网络充足（例如使用腾讯云的 CVM，同地域上传 COS）的情况下，设置成16或32即可，可较充分的利用网络资源
+        // 对于使用公网传输且网络带宽质量不高的情况，建议减小该值，避免因网速过慢，造成请求超时。
+        ExecutorService threadPool = Executors.newFixedThreadPool(32);
+
+        // 传入一个 threadpool, 若不传入线程池，默认 TransferManager 中会生成一个单线程的线程池。
+        TransferManager transferManager = new TransferManager(cosClient, threadPool);
+
+        // 设置高级接口的配置项
+        // 分块上传阈值和分块大小分别为 5MB 和 1MB
+        TransferManagerConfiguration transferManagerConfiguration = new TransferManagerConfiguration();
+        transferManagerConfiguration.setMultipartUploadThreshold(5 * 1024 * 1024);
+        transferManagerConfiguration.setMinimumUploadPartSize(1 * 1024 * 1024);
+        transferManager.setConfiguration(transferManagerConfiguration);
+
+        return transferManager;
+    }
+
+    void shutdownTransferManager(TransferManager transferManager) {
+        // 指定参数为 true, 则同时会关闭 transferManager 内部的 COSClient 实例。
+        // 指定参数为 false, 则不会关闭 transferManager 内部的 COSClient 实例。
+        transferManager.shutdownNow(true);
+    }
+
+    // 创建 COSClient 实例，这个实例用来后续调用请求
+    COSClient createCOSClient() {
+        // 设置用户身份信息。
+        // SECRETID 和 SECRETKEY 请登录访问管理控制台 https://console.cloud.tencent.com/cam/capi 进行查看和管理
+        Credentials credentials = GetCredential();
+        String secretId = credentials.tmpSecretId;
+        String secretKey = credentials.tmpSecretKey;
+        String sessionToken = credentials.sessionToken;
+        COSCredentials cred = new BasicSessionCredentials(secretId, secretKey, sessionToken);
+
+
+        // ClientConfig 中包含了后续请求 COS 的客户端设置：
+        ClientConfig clientConfig = new ClientConfig();
+
+        // 设置 bucket 的地域
+        // COS_REGION 请参照 https://cloud.tencent.com/document/product/436/6224
+
+        clientConfig.setRegion(new Region(regionName));
+
+        // 设置请求协议, http 或者 https
+        // 5.6.53 及更低的版本，建议设置使用 https 协议
+        // 5.6.54 及更高版本，默认使用了 https
+        clientConfig.setHttpProtocol(HttpProtocol.https);
+
+        // 以下的设置，是可选的：
+
+        // 设置 socket 读取超时，默认 30s
+        clientConfig.setSocketTimeout(30 * 1000);
+        // 设置建立连接超时，默认 30s
+        clientConfig.setConnectionTimeout(30 * 1000);
+
+        // 如果需要的话，设置 http 代理，ip 以及 port
+        //clientConfig.setHttpProxyIp("httpProxyIp");
+        //clientConfig.setHttpProxyPort(80);
+        // 生成 cos 客户端。
+        return new COSClient(cred, clientConfig);
+    }
+
+    void showTransferProgress(Transfer transfer) {
+        // 这里的 Transfer 是异步上传结果 Upload 的父类
+        System.out.println(transfer.getDescription());
+
+        // transfer.isDone() 查询上传是否已经完成
+        while (transfer.isDone() == false) {
+            try {
+                // 每 2 秒获取一次进度
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return;
+            }
+
+            TransferProgress progress = transfer.getProgress();
+            long sofar = progress.getBytesTransferred();
+            long total = progress.getTotalBytesToTransfer();
+            double pct = progress.getPercentTransferred();
+            System.out.printf("upload progress: [%d / %d] = %.02f%%\n", sofar, total, pct);
+        }
+
+        // 完成了 Completed，或者失败了 Failed
+        System.out.println(transfer.getState());
+    }
+
+    public Credentials GetCredential() {
+        TreeMap<String, Object> config = new TreeMap<String, Object>();
+
+        try {
+            Properties properties = new Properties();
+            File configFile = new File("src/main/resources/application.properties");
+            properties.load(new FileInputStream(configFile));
+
+            // 云 api 密钥 SecretId
+            config.put("secretId", properties.getProperty("qcloud.ufile.secretid"));
+            // 云 api 密钥 SecretKey
+            config.put("secretKey", properties.getProperty("qcloud.ufile.secretkey"));
+
+            if (properties.containsKey("https.proxyHost")) {
+                System.setProperty("https.proxyHost", properties.getProperty("https.proxyHost"));
+                System.setProperty("https.proxyPort", properties.getProperty("https.proxyPort"));
+            }
+
+            // 设置域名
+            //config.put("host", "sts.internal.tencentcloudapi.com");
+
+            // 临时密钥有效时长，单位是秒
+            config.put("durationSeconds", 1800);
+
+            // 换成你的 bucket
+            config.put("bucket", bucketName);
+            // 换成 bucket 所在地区
+            config.put("region", regionName);
+
+            // 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的具体路径
+            // 列举几种典型的前缀授权场景：
+            // 1、允许访问所有对象："*"
+            // 2、允许访问指定的对象："a/a1.txt", "b/b1.txt"
+            // 3、允许访问指定前缀的对象："a*", "a/*", "b/*"
+            // 如果填写了“*”，将允许用户访问所有资源；除非业务需要，否则请按照最小权限原则授予用户相应的访问权限范围。
+            config.put("allowPrefixes", new String[]{
+                    "*"
+            });
+
+            // 密钥的权限列表。简单上传和分片需要以下的权限，其他权限列表请看 https://cloud.tencent.com/document/product/436/31923
+            String[] allowActions = new String[]{
+                    // 简单上传
+                    "name/cos:PutObject",
+                    "name/cos:PostObject",
+                    // 分片上传
+                    "name/cos:InitiateMultipartUpload",
+                    "name/cos:ListMultipartUploads",
+                    "name/cos:ListParts",
+                    "name/cos:UploadPart",
+                    "name/cos:CompleteMultipartUpload"
+            };
+            config.put("allowActions", allowActions);
+
+             Credentials credentials = CosStsClient.getCredential(config).credentials;
+            return credentials;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException("no valid secret !");
+        }
+    }
+
+    public URL generatePresignedUrl(COSClient cosClient, String bucketName, String key, Date expiration, HttpMethodName method, Map<String, String> headers, Map<String, String> params, Boolean signPrefixMode, Boolean signHost) throws CosClientException {
+        // 调用 COS 接口之前必须保证本进程存在一个 COSClient 实例，如果没有则创建
+// 详细代码参见本页：简单操作 -> 创建 COSClient
+
+
+// 存储桶的命名格式为 BucketName-APPID，此处填写的存储桶名称必须为此格式
+
+// 对象键(Key)是对象在存储桶中的唯一标识。详情请参见 [对象键](https://cloud.tencent.com/document/product/436/13324)
+
+
+// 设置签名过期时间(可选), 若未进行设置则默认使用 ClientConfig 中的签名过期时间(1小时)
+// 这里设置签名在半个小时后过期
+        Date expirationDate = new Date(System.currentTimeMillis() + 30 * 60 * 1000);
+
+// 填写本次请求的参数，需与实际请求相同，能够防止用户篡改此签名的 HTTP 请求的参数
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("param1", "value1");
+
+// 填写本次请求的头部，需与实际请求相同，能够防止用户篡改此签名的 HTTP 请求的头部
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("header1", "value1");
+
+// 请求的 HTTP 方法，上传请求用 PUT，下载请求用 GET，删除请求用 DELETE
+        HttpMethodName method = HttpMethodName.GET;
+
+        URL url = cosClient.generatePresignedUrl(bucketName, key, expirationDate, method, headers, params);
+        System.out.println(url.toString());
+
+// 确认本进程不再使用 cosClient 实例之后，关闭之
+        cosClient.shutdown();
+    }
+}
